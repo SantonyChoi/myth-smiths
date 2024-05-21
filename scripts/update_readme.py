d63ac145c2d75c88ce06c8d0c706954f15982b76
Hoon Choi <santony.choi@gmail.com>
2024-05-21 22:48:08 +0900

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -5,7 +5,7 @@ def update_readme():
     episodes = glob.glob('episodes/*.md')
     episodes.sort()
     with open('README.md', 'w') as readme:
-        readme.write("# 웹소설 에피소드 인덱스\n\n")
+        readme.write("# Episodes\n\n")
         for episode in episodes:
             episode_title = os.path.basename(episode).replace('.md', '')
             readme.write(f"- [{episode_title}]({episode})\n")
